f583b1846ce3dd2dfb5054fe51ba1def943637d3
Helen Graham <Helen@helens-macbook-air.local>
2019-02-16 09:49:09 -0800

    Initial Commit

---
+++ Harvard Westlake app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Harvard Westlake app
+//
+//  Created by Helen Graham on 2/16/19.
+//  Copyright © 2019 Helen Graham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
